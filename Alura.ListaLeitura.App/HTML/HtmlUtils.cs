b8329e2fbfac14a7383be19f026eb6bf9d04af92
David Alexandre <david.paula.dias@everis.com>
2019-09-20 18:36:17 -0300

    Atualização MVC

---
+++ Alura.ListaLeitura.App/HTML/HtmlUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alura.ListaLeitura.App.HTML
+{
+ public   class HtmlUtils
+    {
+        public static string CarregaArquivoHTML(string Arquivo)
+        {
+
+            var nomeCompletoArquivo = $"../../../HTML/{Arquivo}.html";
+            using (var arquivo = File.OpenText(nomeCompletoArquivo))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+    }
+}
